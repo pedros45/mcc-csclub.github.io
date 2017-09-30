745074be367e1283e6e97fd8d299eabc1ac565d7
Noah Teshima <NoahT@users.noreply.github.com>
2017-09-30 00:03:58 -0700

    Header animation
    These changes add the jQuery UI library, which allows us to use additional animations for the landing page or any other part of the site. Calls to old and new animations were chained with .stop() to allow for any current callback to stop if another takes precedence. For instance, if we are in an animation to go to the bottom of the page and we call another to go to the middle, we will stop the current callback to the bottom and go straight to the middle.

--- main.js
+++ main.js
@@ -1,24 +1,52 @@
-$(document).ready(init);
-
-function init() {
-	$("header > ul > li").hover(
-		function() {
-			$(this).stop().animate(
-			{
-				opacity: 1
-			}, {
-				duration: 250,
-				easing: "linear"
+var globalApplication = {
+	init: function() { //initialized for event delegation
+		$("header > ul > li").hover(
+			function() {
+				$(this).stop().animate(
+				{
+					opacity: 1
+				}, {
+					duration: 250,
+					easing: "linear"
+				});
+			},
+			function() {
+				$(this).stop().animate(
+				{
+					opacity: .5
+				}, {
+					duration: 250,
+					easing: "linear"
+				});
 			});
-		},
-		function() {
-			$(this).stop().animate(
+
+		$("header > ul > li > a").on("click", function() {
+			$("header > ul > li > a").removeClass("current"); //selector only affects header
+			$(this).attr("class", "current");
+			var id= $(this).attr("href");
+			$("html, body").stop().animate(
 			{
-				opacity: .5
-			}, {
-				duration: 250,
-				easing: "linear"
+				scrollTop: $(id).offset().top //offset from the top position of the element with specific id
+			},
+			{
+				duration: 1000,
+				easing: "swing"
 			});
 		});
+	}
+};
+$(document).ready(globalApplication.init);
 
+/*
+Currently unused and needs patching. Removes specific attribute from immediate children.
+*/
+function removeChildrenAttribute(parent, attribute, value) {
+	if($(parent).attr(attribute) === value)
+		$(parent).removeAttr(attribute);
+
+	$(parent).children().each(function(index) { //we cannot use a selector parameter, since we don't know the attribute
+		if($(this).attr(attribute) === value)
+			$(this).removeAttr(attribute);
+	});
 }
+
