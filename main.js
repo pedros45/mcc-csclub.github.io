3a27bb1516f0e7339a05555acc455753c946d030
Noah Teshima <NoahT@users.noreply.github.com>
2017-10-01 14:55:58 -0700

    Patched header selection with scrolling events
    The current header is designed to show what section of the landing we are on. This would only work if the links were clicked, so it would show that we are at the top of the page while we scroll to another part. I checked the current location of the scrollbar and compared it to the location of all sections in order to fix this.

--- main.js
+++ main.js
@@ -1,40 +1,78 @@
 var globalApplication = {
 	init: function() { //initialized for event delegation
-		$("header > ul > li").hover(
-			function() {
-				$(this).stop().animate(
-				{
-					opacity: 1
-				}, {
-					duration: 250,
-					easing: "linear"
-				});
-			},
-			function() {
-				$(this).stop().animate(
-				{
-					opacity: .5
-				}, {
-					duration: 250,
-					easing: "linear"
-				});
+		globalApplication.events.document();
+		globalApplication.events.header();
+	},
+	events: {
+		document: function() {
+			setClosestHeaderSection(); //first call on startup;
+			
+			$(document).on("scroll", function() {
+				setClosestHeaderSection();
 			});
 
-		$("header > ul > li > a").on("click", function() {
-			$("header > ul > li > a").removeClass("current"); //selector only affects header
-			$(this).attr("class", "current");
-			var id= $(this).attr("href");
-			$("html, body").stop().animate(
-			{
-				scrollTop: $(id).offset().top //offset from the top position of the element with specific id
-			},
-			{
-				duration: 1000,
-				easing: "swing"
-			});
-		});
+			/*
+			checks href of each anchor in header, finds id with that href, and returns the closest id.
+			*/
+			function setClosestHeaderSection() {
+				var min, //undefined
+				returnId;
+				$("header > ul > li > a").each(function(index, value) {
+					var currentId = $(this).attr("href"), //id of the element we should check
+					sectionVerticalPosition = $(currentId).offset().top, //section distance from top
+					scrollVerticalPosition = $("html, body").scrollTop(); //current distance from top
+					distance = Math.abs(sectionVerticalPosition - scrollVerticalPosition); //current distance from the scroll position and the section
+					
+					if(min === undefined || distance < min) {
+						min = distance;
+						returnId = currentId;
+					}
+				});
+				$("header > ul > li > a").removeClass("current"); //we should save the current reference to the class to avoid iterating over this selector
+				$("header > ul > li ").find("[href='" + returnId + "']").attr("class", "current");
+			}
+		},
+		header: function() {
+			$("header > ul > li").hover(
+				function() {
+					$(this).stop().animate(
+					{
+						opacity: 1
+					}, {
+						duration: 250,
+						easing: "linear"
+					});
+				},
+				function() {
+					$(this).stop().animate(
+					{
+						opacity: .5
+					}, {
+						duration: 250,
+						easing: "linear"
+					});
+				});
+
+			$("header > ul > li > a").on("click",
+				function() {
+					$("header > ul > li > a").removeClass("current"); //selector only affects header
+
+					$(this).attr("class", "current");
+					var id= $(this).attr("href");
+
+					$("html, body").stop().animate(
+					{
+						scrollTop: $(id).offset().top //offset from the top position of the element with specific id
+					},
+					{
+						duration: 1000,
+						easing: "swing"
+					});
+				});
+		}
 	}
 };
+
 $(document).ready(globalApplication.init);
 
 /*
