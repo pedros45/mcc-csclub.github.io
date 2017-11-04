737e70f64990ccdb8aebb3767b87c5865bb95c60
ben-var <ben.var@outlook.com>
2017-11-03 18:38:42 -0700

    Revert "Fixed projects section styling (#22)"
    This reverts commit 777a4b039feb31d67bc15394ba842febb31d8c5d.

--- jsonParse.js
+++
@@ -1,9 +0,0 @@
-/*document.onload = function() {
-  console.log(document.getElementById('projects'));
-  console.log('loaded');
-}
-
-*/
-window.onload = function() {
-  console.log(JSON.parse(document.querySelector('#projects > div')));
-}
